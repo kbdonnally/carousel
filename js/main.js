daca66aebd2164824940dd5ab850f2adc2db20a0
Katherine <kbd3ax@virginia.edu>
2017-11-10 14:04:18 -0500

    Set up initial styles/text/vars
    -Modify HTML to have dummy divs and simplified class names.
    
    -Add CSS styles based on items/show-test files.
    
    -Set up variables in JS.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 (function() {
-	// do stuff
-})
+	var slide,
+		slideTrack,
+		gallery,
+		btnLeft,
+		btnRight;
+})();
+
+
+  /*<div class="slide-wrapper-parent">
+        <!--a class="collection-link" href="http://localhost/collections/show/11" style="float:left;"><< Back to Collection</a-->
+        <div class="slide-wrapper-visible" style="clear:both;"> <!-- border/padding/margin styles -->
+            <button class="carousel-btn left">&laquo;</button>
+            <button class="carousel-btn right">&raquo;</button>
+            <div class="slide-wrapper-mask">
+                <div class="slide-container">
+                    <!-- current item -->
+                    <div class="slide">Item 1</div>*/
