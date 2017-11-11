3676351ccf60aac54a2197ba37b3a1fc0d288278
Katherine <kbd3ax@virginia.edu>
2017-11-11 18:37:37 -0500

    Add functionality on gallery nav buttons
    -Stable forward/backward buttons that also re-align slides if you pan
     and then use the buttons to navigate. By default would translate by
     gallery visible width; if slide isn't flush w/ gallery on the left, it
     translates by (visible width - the width by which the leftmost slide is
     misaligned).
    
    -NB: need to add 'tap' event listeners to buttons so am not misusing
         mouse events needlessly.
    
    -Explanations:
    1.) index.html
      -Comment out first div so there are 49 rather than 50.
      -(Reason: test edge case of icky numbers to ensure calculations work).
    
    2.) main.js
      -Add properties for padding and visible width on gallery var
      -Add property for 1 slide = x% of total collection on slide var
      -Add event listeners for clicking buttons.

--- js/main.js
+++ js/main.js
@@ -6,35 +6,41 @@
 		btnLeft,
 		btnRight;
 
-	// initialize variables
 	slideList 		= document.getElementsByClassName('slide');
 	slideTrack 		= document.querySelector('.slide-container');
 	btnLeft 		= document.querySelector('.left');
 	btnRight 		= document.querySelector('.right');
-
 	gallery 		= {
 					  elem: 	 document.querySelector('.slide-wrapper-visible'),
 					  get width() {
 						return this.elem.offsetWidth;
+					  },
+					  get padding() {
+					  	return parseInt(window.getComputedStyle(this.elem, null).paddingLeft);
+					  },
+					  get visibleWidth() {
+					  	return this.width - (2 * this.padding);
 					  }
 					  };
-
 	slide 			= {
 					  height: 	 slideList[0].offsetHeight,
 					  divWidth:  slideList[0].offsetWidth,
 					  margin: 	 parseInt(window.getComputedStyle(slideList[0], null).marginLeft),
+					  percentOfTotal: 100 / slideList.length,
 					  get width() {
 						  return this.divWidth + (2 * this.margin);
 					  }
 					  };
 	
-
-	// set track width
+	// set track width, margin, translation
 	slideTrack.style.width = slide.width * slideList.length + 'px';
 	console.log(slideTrack.style.width);
+	console.log(slide.percentOfTotal);
+	console.log(1 == .99999999999.toFixed(2));
+	//slideTrack.style.marginLeft = 0;
+	slideTrack.setAttribute('data-translateX', "0");
 
-	// create slide manager for touch events
-	// in English: can recognize pan and swipe events on slide track
+	// recognize pan and swipe events on slide track
 	var sliderManager = new Hammer.Manager(slideTrack, {
 	recognizers: [
 		[Hammer.Pan,{ threshold:0 }],
@@ -42,14 +48,13 @@
 	]
 	});
 
-	// pan slides on touch
 	var lastDelta,
 		percentage,
-		moveBy;
+		moveBy,
+		numSlidesVisible;
 
-	slideTrack.style.marginLeft = 0;
-	slideTrack.setAttribute('data-translateX', "0");
 
+	// pan slides on touch
 	sliderManager.on('panmove', function(e) {
 		lastDelta = slideTrack.getAttribute("data-translateX");
 		console.log(lastDelta);
@@ -63,6 +68,32 @@
 		});
 	});
 
+	// enable buttons
+	btnRight.addEventListener('click', carouselBtn, false);
+	btnLeft.addEventListener('click', carouselBtn, false);
 
+	function carouselBtn(e) {
+		lastDelta = slideTrack.getAttribute("data-translateX");
+		numSlidesVisible = Math.floor(gallery.visibleWidth / slide.width);
+		percentage = 100 * numSlidesVisible / slideList.length;
+		if (e.target.classList.contains("right")) {
+			percentage = percentage * -1;
+		}
+		moveBy = percentage + parseFloat(lastDelta);
+		var alignTest = lastDelta / slide.percentOfTotal;
+		console.log(alignTest.toFixed(1));
+		if ((alignTest.toFixed(1) % 1) === 0) {
+			slideTrack.style.transform = `translateX(${moveBy}%)`;
+			slideTrack.setAttribute('data-translateX', moveBy);
+			console.log(moveBy);	
+		} else {
+			console.log(alignTest % 1);
+			moveBy = moveBy - ((alignTest % 1) * slide.percentOfTotal);
+			slideTrack.style.transform = `translateX(${moveBy}%)`;
+			slideTrack.setAttribute('data-translateX', moveBy);
+			console.log(moveBy);
+		}
+		
+	}
 	
 })();
