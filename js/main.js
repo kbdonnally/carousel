516677e59a5d92aae717a393d82bf4c51939f36e
Katherine <kbd3ax@virginia.edu>
2017-11-12 15:14:55 -0500

    Remove console tests, format spacing (readability)

--- js/main.js
+++ js/main.js
@@ -11,33 +11,30 @@
 	btnLeft 		= document.querySelector('.left');
 	btnRight 		= document.querySelector('.right');
 	gallery 		= {
-					  elem: 	 document.querySelector('.slide-wrapper-visible'),
+					  elem: 	 		document.querySelector('.slide-wrapper-visible'),
 					  get width() {
-						return this.elem.offsetWidth;
+										return this.elem.offsetWidth;
 					  },
 					  get padding() {
-					  	return parseInt(window.getComputedStyle(this.elem, null).paddingLeft);
+					  					return parseInt(window.getComputedStyle(this.elem, null).paddingLeft);
 					  },
 					  get visibleWidth() {
-					  	return this.width - (2 * this.padding);
+					  					return this.width - (2 * this.padding);
 					  }
 					  };
 	slide 			= {
-					  height: 	 slideList[0].offsetHeight,
-					  divWidth:  slideList[0].offsetWidth,
-					  margin: 	 parseInt(window.getComputedStyle(slideList[0], null).marginLeft),
-					  percentOfTotal: 100 / slideList.length,
+					  height: 	 		slideList[0].offsetHeight,
+					  divWidth:  		slideList[0].offsetWidth,
+					  margin: 	 		parseInt(window.getComputedStyle(slideList[0], null).marginLeft),
+					  percentOfTotal: 	100 / slideList.length,
 					  get width() {
-						  return this.divWidth + (2 * this.margin);
+						  				return this.divWidth + (2 * this.margin);
 					  }
 					  };
 	
 	// set track width, margin, translation
 	slideTrack.style.width = slide.width * slideList.length + 'px';
-	console.log(slideTrack.style.width);
 	console.log(slide.percentOfTotal);
-	console.log(1 == .99999999999.toFixed(2));
-	//slideTrack.style.marginLeft = 0;
 	slideTrack.setAttribute('data-translateX', "0");
 
 	// recognize pan and swipe events on slide track
@@ -51,17 +48,15 @@
 	var lastDelta,
 		percentage,
 		moveBy,
-		numSlidesVisible;
+		numSlidesVisible,
+		alignTest;
 
 
 	// pan slides on touch
 	sliderManager.on('panmove', function(e) {
 		lastDelta = slideTrack.getAttribute("data-translateX");
-		console.log(lastDelta);
 		percentage = 100 * e.deltaX / (slide.width * slideList.length);
-		console.log(percentage);
 		moveBy = percentage + parseFloat(lastDelta);
-		console.log(moveBy);
 		slideTrack.style.transform = `translateX(${moveBy}%)`;
 		sliderManager.on('panend', function(e) {
 			slideTrack.setAttribute('data-translateX', moveBy);
@@ -80,20 +75,15 @@
 			percentage = percentage * -1;
 		}
 		moveBy = percentage + parseFloat(lastDelta);
-		var alignTest = lastDelta / slide.percentOfTotal;
+		alignTest = lastDelta / slide.percentOfTotal;
 		console.log(alignTest.toFixed(1));
 		if ((alignTest.toFixed(1) % 1) === 0) {
 			slideTrack.style.transform = `translateX(${moveBy}%)`;
-			slideTrack.setAttribute('data-translateX', moveBy);
-			console.log(moveBy);	
+			slideTrack.setAttribute('data-translateX', moveBy);	
 		} else {
-			console.log(alignTest % 1);
 			moveBy = moveBy - ((alignTest % 1) * slide.percentOfTotal);
 			slideTrack.style.transform = `translateX(${moveBy}%)`;
 			slideTrack.setAttribute('data-translateX', moveBy);
-			console.log(moveBy);
-		}
-		
+		}		
 	}
-	
 })();
